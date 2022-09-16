050bb774f538c417e9a7ccfbe69bc63e6f636552
VishalBallal <vishalballal26@gmail.com>
2022-09-16 19:05:20 +0530

    code added for multiplication

--- calculator.py
+++ calculator.py
@@ -8,3 +8,7 @@ print(f'Addition of {a} and {b} is {addition(a,b)}')
 def substraction(a,b):
     return a-b
 print(f'Substraction of {a} and {b} is {substraction(a,b)}')
+
+def multiplication(a,b):
+    return a*b
+print(f'Multiplication of {a} and {b} is {multiplication(a,b)}')
