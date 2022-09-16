485fa4aec23a1e4da1581fbb9800dc6524df768d
VishalBallal <vishalballal26@gmail.com>
2022-09-16 19:33:56 +0530

    code added for division

--- calculator.py
+++ calculator.py
@@ -8,3 +8,7 @@ print(f'Addition of {a} and {b} is {addition(a,b)}')
 def substraction(a,b):
     return a-b
 print(f'Substraction of {a} and {b} is {substraction(a,b)}')
+
+def division(a,b):
+    return a/b
+print(f'Division of {a} and {b} is {division(a,b)}')
